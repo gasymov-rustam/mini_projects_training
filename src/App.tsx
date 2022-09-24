ef1507eb79d29ed5e1a4d5c8426c6621af4a13be
Gasymov Rustam <rustam.gasymov@devalore.com>
2022-09-24 12:47:39 +0300

    feat: create modal window

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
 import { FC, useState } from 'react';
 import cln from './App.module.scss';
 
-interface AppProps {}
+interface ModalWindowProps {
+  visible: boolean;
+  setVisible: () => void;
+}
 
-export const App: FC<AppProps> = () => {
-  const [counter, setCounter] = useState<number>(0);
+const ModalWindow: FC<ModalWindowProps> = ({ setVisible, visible }) => {
+  return (
+    <div className={`${cln.overlay} ${cln.animated} ${visible ? cln.show : ''}`}>
+      <div className={cln.modal}>
+        <svg height='200' viewBox='0 0 200 200' width='200' onClick={setVisible}>
+          <title />
+          <path d='M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z' />
+        </svg>
+        <img src='https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif' alt='' />
+      </div>
+    </div>
+  );
+};
 
-  const handleClick = (isDecrement: boolean) => () => {
-    if (isDecrement) {
-      setCounter((prev) => prev + 1);
-    } else {
-      setCounter((prev) => prev - 1);
-    }
+export const App = () => {
+  const [visible, setVisible] = useState<boolean>(false);
+
+  const handleVisibleClick = () => {
+    setVisible((prev) => !prev);
   };
   return (
     <div className={cln.App}>
-      <div>
-        <h2>Counter: </h2>
-        <h1>{counter}</h1>
-        <button className={cln.minus} onClick={handleClick(false)}>
-          Minus
-        </button>
-        <button className={cln.plus} onClick={handleClick(true)}>
-          Plus
-        </button>
-      </div>
+      <button className={cln['open-modal-btn']} onClick={handleVisibleClick}>
+        ✨ Open window
+      </button>
+      <ModalWindow setVisible={handleVisibleClick} visible={visible} />
     </div>
   );
 };
