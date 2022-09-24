2a6d6b26e2de0651c9c938a8eec9b7e3eff8478c
Gasymov Rustam <rustam.gasymov@devalore.com>
2022-09-24 13:27:17 +0300

    feat: add my realisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,96 @@
-import { FC, PropsWithChildren, useState } from 'react';
+import { FC, PropsWithChildren, useMemo, useState } from 'react';
 import cln from './App.module.scss';
 
-interface ModalWindowProps extends PropsWithChildren {
-  visible: boolean;
-  setVisible: () => void;
+interface IQuestion {
+  title: string;
+  variants: string[];
+  correct: number;
 }
 
-const ModalWindow: FC<ModalWindowProps> = ({ setVisible, visible, children }) => {
+const questions: IQuestion[] = [
+  {
+    title: 'React is ... ?',
+    variants: ['library', 'framework', 'application'],
+    correct: 0,
+  },
+  {
+    title: 'Component is ... ',
+    variants: ['application', 'part of application or page', 'I don`t know'],
+    correct: 1,
+  },
+  {
+    title: 'What is it JSX?',
+    variants: [
+      'It is just HTML',
+      'It is function',
+      'It is same HTML, but with possibility to do JS-код',
+    ],
+    correct: 2,
+  },
+];
+
+const helper = (length: number, step: number) => `${(100 / length) * step}%`;
+
+interface IResult extends PropsWithChildren {
+  setStep: () => void;
+}
+
+const Result: FC<IResult> = ({ setStep }) => {
   return (
-    <div className={`${cln.overlay} ${cln.animated} ${visible ? cln.show : ''}`}>
-      <div className={cln.modal}>
-        <svg height='200' viewBox='0 0 200 200' width='200' onClick={setVisible}>
-          <title />
-          <path d='M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z' />
-        </svg>
-        {children}
-      </div>
+    <div className={cln['result']}>
+      <img src='https://cdn-icons-png.flaticon.com/512/2278/2278992.png' alt='' />
+      <h2>You did 3 answers from 10</h2>
+      <button onClick={setStep}>Try again!</button>
     </div>
   );
 };
 
-export const App = () => {
-  const [visible, setVisible] = useState<boolean>(false);
+interface IGame extends PropsWithChildren {
+  question: IQuestion;
+  setStep: () => void;
+}
 
-  const handleVisibleClick = () => {
-    setVisible((prev) => !prev);
+const Game: FC<IGame> = ({ question, setStep }) => {
+  return (
+    <>
+      <div className={cln.progress}>
+        <div
+          style={{ width: helper(questions.length, question.correct) }}
+          className={cln['progress__inner']}
+        ></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((item) => (
+          <li key={item} onClick={setStep}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export const App = () => {
+  const [step, setStep] = useState<number>(0);
+  const question = useMemo(() => questions[step], [step]);
+
+  const handleClick = () => {
+    if (step > questions.length - 1) {
+      setStep(0);
+    } else {
+      setStep((prev) => prev + 1);
+    }
   };
+
   return (
     <div className={cln.App}>
-      <button className={cln['open-modal-btn']} onClick={handleVisibleClick}>
-        ✨ Open window
-      </button>
-      <ModalWindow setVisible={handleVisibleClick} visible={visible}>
-        <img src='https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif' alt='' />
-      </ModalWindow>
+      {question ? (
+        <Game question={question} setStep={handleClick} />
+      ) : (
+        <Result setStep={handleClick} />
+      )}
+      {/* <Result /> */}
     </div>
   );
 };
