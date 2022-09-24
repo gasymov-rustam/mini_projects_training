0363579d7671f65598643f7353d5f3fbb69e4b59
Gasymov Rustam <rustam.gasymov@devalore.com>
2022-09-24 12:55:21 +0300

    feat: create modal window with children

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { FC, useState } from 'react';
+import { FC, PropsWithChildren, useState } from 'react';
 import cln from './App.module.scss';
 
-interface ModalWindowProps {
+interface ModalWindowProps extends PropsWithChildren {
   visible: boolean;
   setVisible: () => void;
 }
 
-const ModalWindow: FC<ModalWindowProps> = ({ setVisible, visible }) => {
+const ModalWindow: FC<ModalWindowProps> = ({ setVisible, visible, children }) => {
   return (
     <div className={`${cln.overlay} ${cln.animated} ${visible ? cln.show : ''}`}>
       <div className={cln.modal}>
@@ -14,7 +14,7 @@ const ModalWindow: FC<ModalWindowProps> = ({ setVisible, visible }) => {
           <title />
           <path d='M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z' />
         </svg>
-        <img src='https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif' alt='' />
+        {children}
       </div>
     </div>
   );
@@ -31,7 +31,9 @@ export const App = () => {
       <button className={cln['open-modal-btn']} onClick={handleVisibleClick}>
         ✨ Open window
       </button>
-      <ModalWindow setVisible={handleVisibleClick} visible={visible} />
+      <ModalWindow setVisible={handleVisibleClick} visible={visible}>
+        <img src='https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif' alt='' />
+      </ModalWindow>
     </div>
   );
 };
